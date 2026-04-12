1b4913e50a87a8af22aa8219873d9e7a0af962d1
Healare <victorr.goblett@gmail.com>
2026-04-12 16:57:22 +0200

    Test_APP_Fact_IC

---
+++ App_python_facture.py
@@ -0,0 +1,114 @@
+import streamlit as st
+import re
+import pandas as pd
+import pdfplumber
+from collections import namedtuple
+import openpyxl
+
+
+st.title("📄 Traitement de facture")
+
+presta = namedtuple("Prestation", "Numéro_Article Description Quantité Prix_Total")
+
+# Upload du PDF
+pdf_file = st.file_uploader("Uploader le PDF", type="pdf")
+
+if pdf_file:
+
+    text = ""
+    with pdfplumber.open(pdf_file) as pdf:
+        for page in pdf.pages[:]:
+            text += page.extract_text()
+
+    pres_re = re.compile(r"(^\d+\S*)\s(.+?)\s+(\d+)\s+0,\d+.*?(\d+,\d+|\d+)\s+\d+\s*$")
+
+    Article = []
+
+    for ligne in text.splitlines():
+        match = pres_re.search(ligne)
+        if match:
+            num_art = match.group(1)
+            desc = match.group(2)
+            qté = match.group(3)
+            prix_tot = match.group(4)
+
+            Article.append(presta(num_art, desc, qté, prix_tot))
+
+    df = pd.DataFrame(Article)
+
+    if not df.empty:
+        df["Quantité"] = df["Quantité"].astype(int)
+        df["Prix_Total"] = df["Prix_Total"].str.replace(",", ".").astype(float)
+
+        df_grouped = df.groupby(
+            ["Numéro_Article", "Description"], as_index=False
+        ).agg({"Quantité": "sum", "Prix_Total": "sum"})
+
+        st.subheader("📊 Articles détectés")
+        st.dataframe(df_grouped)
+
+        # Sélection utilisateur
+        st.subheader("🛒 Sélection des articles")
+
+        articles = df_grouped["Numéro_Article"].astype(str).tolist()
+        selected_articles = st.multiselect("Choisir les articles", articles)
+
+        df_selection = df_grouped[df_grouped["Numéro_Article"].astype(str).isin(selected_articles)].copy()
+
+        if not df_selection.empty:
+
+            quantites = []
+
+            st.write("### 🛒 Saisie des quantités")
+
+            for i, row in df_selection.iterrows():
+                col1, col2, col3 = st.columns([2, 5, 2])
+
+                with col1:
+                    st.write(f"**{row['Numéro_Article']}**")
+
+                with col2:
+                    st.write(row["Description"])
+
+                with col3:
+                    q = st.number_input(
+                        "Qté",
+                        min_value=0.0,
+                        step=1.0,
+                        key=f"qte_{i}"  # IMPORTANT sinon bug
+                    )
+
+                quantites.append(q)
+
+            df_selection["Quantité Saran"] = quantites
+
+            df_selection["Prix_Unitaire_Remisé"] = (
+                df_selection["Prix_Total"] / df_selection["Quantité"].replace(0, None)
+            ).round(2)
+
+            df_selection["Prix_Total_Saran"] = (
+                df_selection["Prix_Unitaire_Remisé"] * df_selection["Quantité Saran"]
+            ).round(2)
+            df_selection = df_selection[["Numéro_Article","Description","Quantité Saran","Prix_Unitaire_Remisé","Prix_Total_Saran"]]
+            df_selection = df_selection.rename(columns={
+                "Numéro_Article": "Article",
+                "Quantité Saran": "Qté Saran",
+                "Prix_Unitaire_Remisé": "Prix Unitaire (€)",
+                "Prix_Total_Saran": "Total (€)"
+            })
+
+            st.subheader("✅ Résumé")
+            st.dataframe(df_selection, hide_index=True)
+
+            # Export Excel
+            if st.button("📥 Télécharger Excel"):
+                file_name = "selection.xlsx"
+                df_selection.to_excel(file_name, index=False)
+
+                with open(file_name, "rb") as f:
+                    st.download_button(
+                        label="Télécharger le fichier",
+                        data=f,
+                        file_name=file_name,
+                        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                    )
